3770a4dcdb05764d580a3a9547ec9e2e63d1e5de
salah <akilsalaheddine203@gmail.com>
2023-11-29 23:28:24 +0100

    commit

--- login.php
+++ login.php
@@ -5,8 +5,6 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Connexion</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel='stylesheet' type='text/css' media='screen' href='login.css'>
     <script src='main.js'></script>
 </head>
@@ -14,8 +12,7 @@
 
 <?php
 include("connexion.php");
-session_start();
-session_destroy();
+
 
 if (!empty($_POST["Email"]) && !empty($_POST["Password"])) {
     $email = $_POST["Email"];
